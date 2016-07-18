35afe463c28afa0ff1a20adc334d49ca605acdc7
sherif98 <sheco_12345@hotmail.com>
2016-07-18 07:35:32 +0200

    Initial commit

---
+++ app/src/main/java/com/nanodegree/udacity/android/popularmovies/activities/DisplayMoviesActivity.java
@@ -0,0 +1,15 @@
+package com.nanodegree.udacity.android.popularmovies.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nanodegree.udacity.android.popularmovies.R;
+
+public class DisplayMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_movies);
+    }
+}
