06e19e587274a66602faac7884eaf5295c668795
sherif98 <sheco_12345@hotmail.com>
2016-07-22 21:51:19 +0200

    type: feat added divier in tablet master detail layout

--- app/src/main/java/logic/APIKeys.java
+++ app/src/main/java/logic/APIKeys.java
@@ -1,6 +1,6 @@
 package logic;
 
 public interface APIKeys {
-    String RETROFIT = "-the movie database api key-";
-    String YOUTUBE = "youtube api key";
+    String RETROFIT = "0e9f7f1189723013cb25090c1c4e49ae";
+    String YOUTUBE = "AIzaSyAl5Ma7H1DnHFj57bk5HmjhLnNXXOCJF1U";
 }
